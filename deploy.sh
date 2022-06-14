7f342039bbc824c51becde3b122caa8f6ce79f99
Roelfie <58694687+roelfie@users.noreply.github.com>
2022-06-14 12:09:20 +0200

    Added script to deploy to Google Cloud / GKE.
    * tag images with the git SHA to enforce redeployment of new containers
    * some minor fixes in the k8s scripts

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+docker build -t roelfie/fibonacci-client:latest -t roelfie/fibonacci-client:$SHA -f ./client/Dockerfile ./client
+docker build -t roelfie/fibonacci-api:latest    -t roelfie/fibonacci-api:$SHA    -f ./server/Dockerfile ./server
+docker build -t roelfie/fibonacci-worker:latest -t roelfie/fibonacci-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push roelfie/fibonacci-client:latest
+docker push roelfie/fibonacci-api:latest
+docker push roelfie/fibonacci-worker:latest
+
+docker push roelfie/fibonacci-client:$SHA
+docker push roelfie/fibonacci-api:$SHA
+docker push roelfie/fibonacci-worker:$SHA
+
+# Apply changes in the k8s config
+kubectl apply -f k8s
+
+# Ensure that we're deploying the most recent images
+kubectl set image client-deployment client=roelfie/fibonacci-client:$SHA
+kubectl set image server-deployment server=roelfie/fibonacci-api:$SHA
+kubectl set image worker-deployment worker=roelfie/fibonacci-worker:$SHA
+
